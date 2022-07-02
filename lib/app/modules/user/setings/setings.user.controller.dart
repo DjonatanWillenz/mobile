0b25f5c350947dbdba9c0a5c647327ea504a54c3
djonatanwillenz <djonatanhorus@outlook.com>
2022-07-02 17:08:01 -0300

    Implementado melhorias

--- lib/app/modules/user/setings/setings.user.controller.dart
+++ lib/app/modules/user/setings/setings.user.controller.dart
@@ -1,14 +1,27 @@
 import 'package:app/app/data/enum/setting.enum.dart';
+import 'package:app/app/data/repository/user.setting.repository.dart';
+import 'package:app/app/global/singletons/setings.system.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:get/get.dart';
 
-onChange(id, newVal) {
-  print("Teste");
-}
-
 class SetingsUserController extends GetxController {
-  List<UserSettingDto> settings = [
-    UserSettingDto(settingEnum: UserSettingEnum.NOTIFY, enabled: true),
-    UserSettingDto(settingEnum: UserSettingEnum.NOTIFY, enabled: true),
-    UserSettingDto(settingEnum: UserSettingEnum.NOTIFY, enabled: true),
-  ].obs;
+  final repositoty = Get.find<UserSettingRepository>();
+  final formKey = GlobalKey<FormState>();
+
+  onChange(UserSettingEnum setting, bool newVal) {
+    switch (setting) {
+      case UserSettingEnum.NOTIFY:
+        SettingsSystem.instance.setting.notify = newVal;
+    }
+    repositoty.update(SettingsSystem.instance.setting);
+  }
+
+  enabledOption(UserSettingEnum setting) {
+    switch (setting) {
+      case UserSettingEnum.NOTIFY:
+        return SettingsSystem.instance.setting.notify;
+        break;
+    }
+    return false;
+  }
 }
